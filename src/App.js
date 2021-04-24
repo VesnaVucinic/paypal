1233f54734e28bb2e9bd882555180250a11fd9b3
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-04-24 18:49:35 +0100

    add PayPal button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    showPaypal: false
+  };
+
+  showPaypalButtons = () => {
+    this.setState({ showPaypal: true });
+  };
+
+  render() {
+    const { showPaypal } = this.state;
+    if (showPaypal) {
+      return <PaypalButtons />;
+    } else {
+      return (
+        <div className="main">
+          <h2> Buy this amaizing tranch-coat from Tommy </h2>
+          <img alt="HERITAGE SINGLE BREASTED TRENCH COAT" src={TranchCoat} />
+          <h3>
+            <b>£178</b>
+          </h3>
+          <button onClick={this.showPaypalButtons}> Pay </button>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
