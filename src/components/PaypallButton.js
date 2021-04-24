91907ad2f2c4a0883950fbe1e1c4517ceab6e957
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-04-24 20:41:14 +0100

    add createOrder and onApprove functions

--- src/components/PaypallButton.js
+++ src/components/PaypallButton.js
@@ -41,6 +41,49 @@ class PaypalButton extends React.Component {
         }
     }
 
+    // lifecycle method to check for the loaded script.
+    componentWillReceiveProps(nextProps) {
+        const { isScriptLoaded, isScriptLoadSucceed } = nextProps;
+    
+        const scriptJustLoaded =
+          !this.state.showButtons && !this.props.isScriptLoaded && isScriptLoaded;
+    
+        if (scriptJustLoaded) {
+          if (isScriptLoadSucceed) {
+            PayPalButton = window.paypal.Buttons.driver("react", {
+              React,
+              ReactDOM
+            });
+            this.setState({ loading: false, showButtons: true });
+          }
+        }
+    }
+
+    createOrder = (data, actions) => {
+        return actions.order.create({
+          purchase_units: [
+            {
+              description: +"HERITAGE SINGLE BREASTED TRENCH COAT",
+              amount: {
+                currency_code: "GBP",
+                value: 179
+              }
+            }
+          ]
+        });
+    };
+    
+    onApprove = (data, actions) => {
+        actions.order.capture().then(details => {
+          const paymentData = {
+            payerID: data.payerID,
+            orderID: data.orderID
+          };
+          console.log("Payment Approved: ", paymentData);
+          this.setState({ showButtons: false, paid: true });
+        });
+    };
+    
 }
   
 export default scriptLoader(`https://www.paypal.com/sdk/js?client-id=${CLIENT_ID}`)(PaypalButton);
